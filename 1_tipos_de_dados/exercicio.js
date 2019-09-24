551ab2db797f67011b99d27fc1362201a1483b4c
mondroidz <monnippon@gmail.com>
2019-09-23 22:27:25 -0300

    exercicios

--- 1_tipos_de_dados/exercicio.js
+++ 1_tipos_de_dados/exercicio.js
@@ -1,1 +1,18 @@
 //resposta aqui
+
+
+//guardar dados
+const cliente = {
+ nome : "Monica",
+ idade : 35,
+ primeiraVez : "true",
+ favoritos : ['mosaico', 'sapatos', 'café']
+}
+
+console.log(cliente, 'cliente')
+
+// const nome = prompt("Digite seu nome");
+// const idade = prompt('Inclua sua idade');
+// const primeiraVez = prompt('sua primeira vez aqui?');
+// let favoritos = []
+
--- 2_op_ar/exercicio.js
+++ 2_op_ar/exercicio.js
@@ -1,1 +1,32 @@
 //resposta aqui
+
+ var r = require("readline-sync");
+
+// var a = parseInt(r.question("Digite 1 ou 2"));
+
+// console.log(a);
+
+const listProd = [
+    {
+        nome: "chocolate",
+        valor: 12.00
+    },
+    {
+        nome : "jujuba",
+        valor : 3.00
+    },
+    {
+        nome : 'chiclete',
+        valor : 2.80
+    }
+] 
+const idPosicao = parseInt(r.question("Digite um número de 0 a 2"));
+
+const idPorc = parseInt(r.question("qual o desconto em %?"));
+
+console.log(listProd[2])
+
+
+
+
+
